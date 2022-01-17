e1dc5f6d6a70daefe9a6cdf4e3a423fd81ed3724
unknown <tukhtasin02@gmail.com>
2022-01-17 17:12:03 +0500

    some js exercises

---
+++ jsexercises.js
@@ -0,0 +1,32 @@
+// === #1
+//  Write JS programm to cheak 2 numbers and return
+//  true if one of them is 100 or if the sum of 2 numbers is 100
+
+// const isEqualTo100 = (a,  b) => {
+//     if (a === 100 || b === 100) console.log(true);
+//      else if (a + b === 100) console.log(true);
+//      else console.log(false);
+// }
+// isEqualTo100(100,0)
+
+// ===2#
+// Wrie a JavaScript program to get extension of a filename 
+
+// const getFilenameEx = (str) => str.slice(str.lastIndexOf('.'));
+
+// console.log(getFilenameEx('index.coge.html'));
+
+// ===3#
+//convert to decimal or binary the given number
+
+// function Binary(num , type) {
+//   switch(type){
+//       case 'toDecimal' : {
+//           return parseInt(num,2);
+//       }
+//       case 'toBinary' : {
+//           return num.toString(2);
+//       }
+//     }
+// }
+// console.log(Binary(110, 'toBinary'));
